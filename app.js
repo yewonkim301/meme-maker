3bad2428a59340ff2d41f87f274c2c770078d5f6
yewonkim301 <yeowne301@gmail.com>
2023-05-09 17:21:32 +0900

    Meme-maker 2023 update

---
+++ app.js
@@ -0,0 +1,136 @@
+const textMode = document.getElementById('text-mode');
+const textFont = document.getElementById('text-font');
+const textSize = document.getElementById('text-size');
+const saveBtn = document.getElementById('save');
+const textInput = document.getElementById('text');
+const fileInput = document.getElementById('file');
+const modeBtn = document.getElementById('mode-btn');
+const resetBtn = document.getElementById('reset-btn');
+const eraseBtn = document.getElementById('eraser-btn');
+const colorOptions = Array.from(document.getElementsByClassName('color-option'));
+const color = document.getElementById('color');
+const lineWidth = document.getElementById('line-width');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
+ctx.lineCap = 'round';
+let isPainting = false;
+let isFilling = false;
+
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY)
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting(event) {
+    isPainting = true;
+}
+
+function cancelPainting(event) {
+    isPainting = false;    
+    ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+    ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+    ctx.strokeStyle = ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = ctx.fillStyle = color.value = colorValue;
+}
+
+function onModeClick() {
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = '🪣 Paint'
+    } else {
+        isFilling = true;
+        modeBtn.innerText = '🖌️ Draw'
+    }
+}
+
+function onCanvasClick() {
+    if(isFilling){
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+
+function onResetClick() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+    ctx.strokeStyle = 'white';
+    isFilling = false;
+    modeBtn.innerText = '🪣 Paint';
+}
+
+function onFileChange(event) {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = function() {
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        fileInput.value = null;
+    }
+}
+
+function onDoubleClick(event) {
+    ctx.save();
+    if (text !== '') {
+        const text = textInput.value;
+        const size = textSize.value;
+        const font = textFont.value;
+        const mode = textMode.value;
+        ctx.lineWidth = 1;
+        ctx.font = `${size}px ${font}`;
+        if(mode === 'fill') {
+            ctx.fillText(text, event.offsetX, event.offsetY);           
+        } else {
+            ctx.strokeText(text, event.offsetX, event.offsetY);
+        }
+        ctx.restore();
+    }
+}
+
+function onSaveClick() {
+    const url = canvas.toDataURL();
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'myDrawing.png';
+    a.click();
+}
+
+canvas.addEventListener('dblclick', onDoubleClick);
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', startPainting);
+canvas.addEventListener('mouseup', cancelPainting);
+canvas.addEventListener('mouseleave', cancelPainting);
+canvas.addEventListener('click', onCanvasClick);
+lineWidth.addEventListener('change', onLineWidthChange);
+color.addEventListener('change', onColorChange);
+
+colorOptions.forEach(color => color.addEventListener('click', onColorClick))
+
+modeBtn.addEventListener('click', onModeClick);
+resetBtn.addEventListener('click', onResetClick);
+eraseBtn.addEventListener('click', onEraserClick);
+fileInput.addEventListener('change', onFileChange);
+saveBtn.addEventListener('click', onSaveClick);
